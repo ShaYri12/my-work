1f5bc747ffe4bc06f9f188e658b7cc156c7fa7a8
ShaYri12 <sharjeelb60@gmail.com>
2024-09-19 22:54:15 +0500

    deploy fix

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Task } from "../assets/tasksData";
 import TasksTable from "../components/TasksTable";
 import initialTasksData from "../assets/tasksData";
